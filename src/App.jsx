e0c7bab1550ad0754cead94aa6188d6e3edf9c0d
AkramExp <akram.exp02@gmail.com>
2023-11-12 19:15:45 +0530

    create home component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
 import React from "react";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./ui/Home";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+]);
 export default function App() {
-  return <div>App</div>;
+  return <RouterProvider router={router} />;
 }
