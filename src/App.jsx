52a1b7c9a9f340f70f6d050525d3d3239288702d
AkramExp <akram.exp02@gmail.com>
2023-11-13 12:46:39 +0530

    added path to menu page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import React from "react";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Home from "./ui/Home";
+import AppLayout from "./ui/AppLayout";
+import Menu from "./features/menu/Menu";
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <Home />,
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "menu",
+        element: <Menu />,
+      },
+    ],
   },
 ]);
 export default function App() {
