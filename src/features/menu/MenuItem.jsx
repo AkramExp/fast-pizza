16fa8861406a1eff0cb85a1fa18b0460b86cf5df
AkramExp <akram.exp02@gmail.com>
2023-11-13 23:30:28 +0530

    minor changes in menu items

--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -7,16 +7,21 @@ export default function MenuItem({ pizza }) {
   return (
     <li className="flex gap-4 py-2 ">
       <img
-        className="w-24
-      "
+        className={`w-24 ${soldOut && "grayscale opacity-70"}`}
         src={imageUrl}
         alt=""
       />
       <div className="flex flex-col grow">
         <p className="font-medium">{name}</p>
-        <p className="text-sm">{ingredients}</p>
+        <p className="text-sm capitalize italic text-stone-00 text-stone-500">
+          {ingredients.join(", ")}
+        </p>
         <div className="flex justify-between mt-auto items-center text-sm">
-          <p>{unitPrice}</p>
+          {soldOut ? (
+            <p className="font-semibold text-stone-500 uppercase">Sold Out</p>
+          ) : (
+            <p className="text-stone-700">{unitPrice}</p>
+          )}
           <Button type="small">Add to Cart</Button>
         </div>
       </div>
