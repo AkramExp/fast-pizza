1bff303951729a002e9720e3b33d656a1105c043
AkramExp <akram.exp02@gmail.com>
2023-11-13 22:36:47 +0530

    seperated username component

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import SearchOrder from "../features/order/SearchOrder";
+import Username from "../features/user/Username";
 
 export default function Header() {
   return (
@@ -7,10 +9,8 @@ export default function Header() {
       <Link to="/" className="tracking-widest uppercase">
         Fast Pizza
       </Link>
-      <input
-        className="input bg-yellow-100 w-[8.5rem] sm:w-72 placeholder:text-stone-700 focus:sm:w-80 transition-all duration-300"
-        placeholder="Search Order"
-      />
+      <SearchOrder />
+      <Username />
     </nav>
   );
 }
