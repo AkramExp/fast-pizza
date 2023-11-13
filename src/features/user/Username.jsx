8f46984a2cd611f2d9b5cec53a7b803f665f9606
AkramExp <akram.exp02@gmail.com>
2023-11-13 22:36:20 +0530

    created username component

---
+++ src/features/user/Username.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Username() {
+  return (
+    <div className="hidden uppercase text-sm font-semibold md:block tracking-wide">
+      Akram Ali
+    </div>
+  );
+}
