6daeb091f3833035d27a6cd9b0c18477249e2420
AkramExp <akram.exp02@gmail.com>
2023-11-15 17:27:01 +0530

    created userSlice

--- store.js
+++ store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartSlice from "./src/features/cart/cartSlice";
+import userSlice from "./src/features/user/userSlice";
 
 const store = configureStore({
   reducer: {
     cart: cartSlice,
+    user: userSlice,
   },
 });
 
