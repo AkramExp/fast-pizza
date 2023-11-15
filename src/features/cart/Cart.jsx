569af616336b848ec7636eadfb14b5becd086570
AkramExp <akram.exp02@gmail.com>
2023-11-15 19:44:33 +0530

    finished cart component

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -3,15 +3,18 @@ import LinkButton from "../../ui/LinkButton";
 import CartItem from "./CartItem";
 import { useSelector } from "react-redux";
 import { getCart } from "./cartSlice";
+import EmptyCart from "./EmptyCart";
 
 export default function Cart() {
   const cart = useSelector(getCart);
 
+  if (!cart.length) return <EmptyCart />;
+
   return (
-    <div className="mt-3">
+    <div className="mt-3 py-2">
       <LinkButton to="-1">&larr; Back to Menu</LinkButton>
-      <h1 className="mt-8 font-semibold text-lg mb-8">Your Cart, Akram</h1>
-      <ul className="divide-y divide-stone-400 border-b space-y-2">
+      <h1 className="mt-8 font-semibold text-lg mb-4">Your Cart, Akram</h1>
+      <ul className="divide-y divide-stone-200 border-b">
         {cart.map((pizza) => (
           <CartItem pizza={pizza} key={pizza.pizzaId} />
         ))}
--- src/features/cart/CartItem.jsx
+++ src/features/cart/CartItem.jsx
@@ -1,11 +1,27 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { formatCurrency } from "../../../helpers";
+import UpdateItemQuantity from "../cart/UpdateItemQuantity";
+import DeleteItem from "../cart/DeleteItem";
+import { getCurrentQuantityById } from "./cartSlice";
 
 export default function CartItem({ pizza }) {
   const { pizzaId, name, quantity, totalPrice } = pizza;
+  const currentQuantity = useSelector(getCurrentQuantityById(pizzaId));
 
   return (
-    <div>
-      {quantity} x {name}
+    <div className="py-4 flex items-center justify-between">
+      <p>
+        {quantity} x {name}
+      </p>
+      <div className="flex items-center justify-between gap-6">
+        <p className="font-semibold text-sm">{formatCurrency(totalPrice)}</p>
+        <UpdateItemQuantity
+          pizzaId={pizzaId}
+          currentQuantity={currentQuantity}
+        />
+        <DeleteItem pizzaId={pizzaId} />
+      </div>
     </div>
   );
 }
