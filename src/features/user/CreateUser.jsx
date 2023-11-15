03490a1cf10856af8b3c95e04a7afa07db951881
AkramExp <akram.exp02@gmail.com>
2023-11-15 18:33:05 +0530

    dynamic username

--- src/features/user/CreateUser.jsx
+++ src/features/user/CreateUser.jsx
@@ -1,11 +1,25 @@
 import React, { useState } from "react";
 import Button from "../../ui/Button";
+import { useDispatch } from "react-redux";
+import { updateName } from "../user/userSlice";
+import { useNavigate } from "react-router-dom";
 
 export default function CreateUser() {
   const [username, setUsername] = useState("");
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!username) return;
+
+    dispatch(updateName(username));
+    navigate("/menu");
+  }
 
   return (
-    <form className="flex flex-col items-center">
+    <form className="flex flex-col items-center" onSubmit={handleSubmit}>
       <p
         className="mb-3 text-sm md:text-base
        text-stone-600"
@@ -17,13 +31,9 @@ export default function CreateUser() {
         placeholder="Your full name"
         value={username}
         onChange={(e) => setUsername(e.target.value)}
-        className="input mb-8 w-72"
+        className="input mb-8 w-72 py-3"
       />
-      {username && (
-        <Button to="menu" type="primary">
-          Start Ordering
-        </Button>
-      )}
+      {username !== "" && <Button type="primary">Start Ordering</Button>}
     </form>
   );
 }
