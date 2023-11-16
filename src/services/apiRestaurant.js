31588979e2ae2571f3db5b5b7397102b5cf952d6
AkramExp <akram.exp02@gmail.com>
2023-11-16 11:46:29 +0530

    added create order function

--- src/services/apiRestaurant.js
+++ src/services/apiRestaurant.js
@@ -9,3 +9,21 @@ export async function getMenu() {
 
   return data;
 }
+
+export async function createOrder(newOrder) {
+  try {
+    const res = await fetch(`${API_URL}/order`, {
+      method: "POST",
+      body: JSON.stringify(newOrder),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (!res.ok) throw new Error();
+    const { data } = await res.json();
+    return data;
+  } catch {
+    throw Error("failed creating your order");
+  }
+}
