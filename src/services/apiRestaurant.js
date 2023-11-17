b6a65b49a5f6b7a479dc47e6bece167eba4c5b76
AkramExp <akram.exp02@gmail.com>
2023-11-17 23:21:15 +0530

    created getOrder function

--- src/services/apiRestaurant.js
+++ src/services/apiRestaurant.js
@@ -27,3 +27,11 @@ export async function createOrder(newOrder) {
     throw Error("Failed creating your order");
   }
 }
+
+export async function getOrder(id) {
+  const res = await fetch(`${API_URL}/order/${id}`);
+  if (!res.ok) throw new Error(`Could not find order with id ${id}`);
+
+  const { data } = res.json();
+  return data;
+}
